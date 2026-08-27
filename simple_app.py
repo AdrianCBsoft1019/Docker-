69429079e9d87a66bf8889d0791cc32af7998b48
roderick correa <adrian.cb.soft1019@gmail.com>
2026-08-26 22:54:06 -0500

    correccion dockerfile

--- simple_app.py
+++ simple_app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, redirect, url_for, os
+from flask import Flask, request, render_template, redirect, url_for
+import os
 import pymysql
 
 app = Flask(__name__)
